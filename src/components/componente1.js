348b0566c895a889980ec698e2ff8a7315417a12
Jorge Leonardo Guepud Bravo <jlguepudb@unal.edu.co>
2022-10-18 20:00:48 -0500

    Agregando componentes

---
+++ src/components/componente1.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Component} from "react";
+
+class Component1 extends Component {
+    render(){
+        return(
+            <div className="hijo1">
+                <h1> Bienvenida Tanita  </h1>
+            </div>
+        )
+    }
+}
+
+export {Component1};
---
+++ src/components/componente2.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Componente2 extends React.Component{
+    render(){
+        return(
+            <h1> Clase de React </h1>
+        )
+    }
+}
+
+export default Componente2; 
